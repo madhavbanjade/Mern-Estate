d5005e54e37272d532f0dbda24e87bdba094965c
madhavbanjade <madhavbanjade005@gmail.com>
2024-09-08 22:06:21 +0545

    feat server

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div
+        className="
+        flex justify-between item-center  max-w-6xl p-3"
+      >
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ff">
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 padding-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus: outline-none w-26 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to={"/"}>
+            <li className=" hidden: sm:inline text-slate-700  hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="hidden: sm:inline text-slate-700 hover:underline cursor-pointer ">
+              About Us
+            </li>
+          </Link>
+          <Link to={"/sign-in"}>
+            <li className=" text-slate-700 hover:underline cursor-pointer">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
