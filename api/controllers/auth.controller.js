d31f70c8351d8757d09b2c9f4579ba9b0baa1170
madhavbanjade <madhavbanjade005@gmail.com>
2024-09-10 12:12:30 +0545

    sign-up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ userName, email, password: hashedPassword });
   try {
--- api/router/auth.user.js
+++ api/router/auth.user.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { signup } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
-authRouter.route("/signup").post(signup);
+authRouter.route("/sign-up").post(signup);
 
 export default authRouter;
