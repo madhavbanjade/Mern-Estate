9a9021442a641d80ce7232fbffc62514607c4ea8
madhavbanjade <madhavbanjade005@gmail.com>
2024-10-18 11:11:23 +0545

    complete home and about page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,7 +36,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.body.id !== req.params.id) {
+  if (req.user.id !== req.params.id) {
     return next(errorHandler(401, "You can only delete your own account!"));
   }
   try {
