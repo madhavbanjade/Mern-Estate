c2f83ba9786acb8e169cd5322f2e7f78ca312140
madhavbanjade <madhavbanjade005@gmail.com>
2024-10-21 19:45:27 +0545

    kkk

--- api/index.js
+++ api/index.js
@@ -3,42 +3,45 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./router/user.router.js";
 import authRouter from "./router/auth.user.js";
-import cookieParser from "cookie-parser";
 import listingRouter from "./router/listing.router.js";
-
+import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to mongoDB!");
+    console.log("Connected to MongoDB!");
   })
-  .catch((error) => {
-    console.log(error);
-    // console.log("Something went wrong!!");
+  .catch((err) => {
+    console.log(err);
   });
 
+const __dirname = path.resolve();
 
 const app = express();
 
-//this line is responsible for giving the input data from postman or any is you do not do so y,ou caught an error called undefined..
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-//from here we can define our router the url must be from here inside "/"..
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
-//This is middleware where it throw an error if there any while hitting the api.
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error";
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
