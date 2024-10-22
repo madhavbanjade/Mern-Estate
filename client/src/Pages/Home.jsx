b69ec5397a5146c9ff046d9cddaca247ef85db14
madhavbanjade <madhavbanjade005@gmail.com>
2024-10-22 19:02:51 +0545

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
     <div>
       {/* top
        */}
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto mt-[50px]">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
           Discover your <span className="text-slate-500">Future</span> <br />
           home today
